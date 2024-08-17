81c692f00dd64bbbe17786dad50886968c6e1840
sadikpathan9876 <65956063+sadikpathanprojects@users.noreply.github.com>
2024-08-17 19:17:58 +0530

    Update 6nestedifelse.sh

--- 6nestedifelse.sh
+++ 6nestedifelse.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-a = 10
-b = 20
-c = 5
+a=10
+b=20
+c=5
 
-if [$a -lt  $b]
+if [ $a -lt  $b ]
 then
-if [$a -lt $c]
+if [ $a -lt $c ]
   then
-  echo “a is less than b and c”
+  echo 'a is less than b and c'
       else 
-  echo “a is less than b and greater than c”
+  echo 'a is less than b and greater than c'
 fi
-else if [$a -gt $b]
-then 
-   echo “a is greater than b” 
+
+elif [ $a -gt $b ];then 
+   echo 'a is greater than b' 
 else 
-    echo “a and b are equal.”
+    echo 'a and b are equal'
 fi
+
