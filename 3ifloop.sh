1340f970048018ca0fb67fdd0543345e81d79b68
sadikpathan9876 <65956063+sadikpathanprojects@users.noreply.github.com>
2024-08-17 19:01:42 +0530

    Update 3ifloop.sh

--- 3ifloop.sh
+++ 3ifloop.sh
@@ -2,11 +2,11 @@
 a=10 
 
 
-if [ $a == 10 ]
+if [ $a = 10 ]
 then 
-       echo “a is equal to 10” 
+       echo 'a is equal to 10'
 fi
 if [ $a != 10 ]
    then 
-       echo “a is not equal to 10”
+       echo 'a is not equal to 10'
 fi
