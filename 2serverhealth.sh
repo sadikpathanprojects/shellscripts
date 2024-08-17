10c6077e94c27016ec861998e73bb1d32979ecb3
sadikpathan9876 <65956063+sadikpathanprojects@users.noreply.github.com>
2024-08-17 18:59:01 +0530

    Update 2serverhealth.sh

--- 2serverhealth.sh
+++ 2serverhealth.sh
@@ -7,5 +7,4 @@ set -o pipefail  # exits the script when there is a pipefail
 df -hT
 free -g
 nproc
-ps -ef | grep -i amazon | awk -F" " '{print $2}'    #prints only the pid of the amazon processes.
-
+ps -ef | pgrep -i amazon | awk -F" " '{print $2}'    #prints only the pid of the amazon processes.
